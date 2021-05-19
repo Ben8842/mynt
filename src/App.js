3762c043353b8f6d14e0cda59e14cbd64e7211f8
Ben8842 <z87e99b44r55a@gmail.com>
2021-05-19 19:54:13 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Background from "./img/MYNTone.jpeg";
+import logomynt from "./img/logoM.PNG";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: false,
+    };
+  }
+
+  togglemenu() {
+    console.log("hello toggle");
+    if (this.state.menu === true) {
+      this.setState({ menu: false });
+    } else if (this.state.menu === false) {
+      this.setState({ menu: true });
+    }
+    console.log(this.state.menu);
+  }
+
+  render() {
+    const menuextended = (
+      <div className="header">
+        <div className="left">
+          <div> Home</div>
+          <div>News & Events</div>
+          <div> About Us</div>
+          <div>Contact Us</div>
+          <div>Boards & Committees</div>
+        </div>
+        <div className="right">
+          <div>DATA & RESEARCH</div>
+          <div>LEGISLATIVE FISCAL IMPACTS</div>
+          <div>GEOGRAPHY & MAPPING</div>
+          <div>PROGRAMS & SERVICES</div>
+        </div>
+      </div>
+    );
+
+    return (
+      <div className="App">
+        <div className="header">
+          <div className="left"></div>
+          <div className="right">
+            <div className="rightlink">ABOUT US</div>
+            <div className="rightlink">EVENTS</div>
+            <div className="rightlink">BOARDS & COMMITTEES</div>
+          </div>
+        </div>
+        {this.state.menu ? menuextended : null}
+
+        <div
+          className="searchbanner"
+          style={{
+            backgroundImage: `url(${Background})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <h6>
+            Providing independent research, analysis and resources to facilitate
+            informed policy decisions and administration of services
+          </h6>
+
+          <div>
+            <input id="searchfield" placeholder="Search rfa.sc.gov"></input>
+            <button id="search"></button>
+          </div>
+        </div>
+        <div className="primary">
+          <button className="boxbutton"></button>
+          <button className="boxbutton"></button>
+          <button className="boxbutton"></button>
+          <button className="boxbutton"></button>
+        </div>
+        <div className="mainlinks">
+          <h3>POPULAR SERVICES AND LINKS</h3>
+        </div>
+        <div className="footer">
+          <div className="leftfooter">
+            <h6>South Carolina</h6>
+            <h6>Revenue and Fiscal Affairs Office</h6>
+            <div
+              className="logodark"
+              style={{
+                backgroundImage: `url(${logomynt})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </div>
+          <div className="middlefooter">
+            <h6>Privacy Statement</h6>
+            <h6>FOIA</h6>
+            <h6>Disclosures and Reporting</h6>
+            <h6>Report Fraud</h6>
+            <h6>Accessibility</h6>
+            <h6>Contact Us</h6>
+          </div>
+          <div className="rightfooter">
+            <h6>MAIN OFFICE</h6>
+            <h6>GEODETIC SURVEY SECTION</h6>
+            <h6>Disclosures and Reporting</h6>
+            <h6>HEALTH AND DEMOGRAPHICS DIVISION</h6>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
