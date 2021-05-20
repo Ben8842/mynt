4c1e79b437c6150e339e633882b8a612606a0558
Ben8842 <z87e99b44r55a@gmail.com>
2021-05-20 12:22:24 -0400

    text addition

--- src/App.js
+++ src/App.js
@@ -23,15 +23,70 @@ class App extends Component {
     console.log(this.state.menu);
   }
 
+  toggleContent() {
+    console.log("hello toggle");
+  }
+
   render() {
+    const whoWeAre = (
+      <div>
+        PAYMYNT is a movile commerce and fintech company based in greater
+        Boston.
+      </div>
+    );
+
+    const whatWeDo = (
+      <div>
+        PAYMYNT provides consumers with a better way to Shop, Earn, Spend and
+        Invest.
+      </div>
+    );
+
+    const whyWeDo = (
+      <div>
+        PAYMYNT is developing an unified mobile commerce platform to
+        assist/help/guide/ the underserved and underbanked improve their
+        financial well-being. Currently, 55 million Americans (15% of the U.S
+        population) live paycheck to paycheck and are unable to utilize
+        traditional banking services.
+      </div>
+    );
+
+    const howWeDo = (
+      <div>
+        The PAYMYNT platform has many unique features beginnning with URSA, a
+        patent-pending univerals rewards shopping app that earns consumers
+        "Found Money" refunds which comes in the form of cash back and MYNT
+        crypto rewards for shopping online or in-store. The "Found Money'
+        consumers earn, can seamlessly be invested into INVESTMYNT's high yield
+        micro-investing products. MYNT's stable token rewards have many uses,
+        such as a medium of exchange and a store of value. MYNT's can also be
+        transferred ontio the MYNT Bankless Smart Card and spent wherever Visa
+        is accepted or held on as a cold storage wallet. To further our
+        initiatives, PAYMYNT will be introducting the PAYMYNT BankCard which
+        members can easily entroll by using their Found Money as their opening
+        balance and can simply transfer their cash back balances each time they
+        shop and earn. "
+      </div>
+    );
+
+    const motto = (
+      <div>
+        <div>Invest the Cash, Spend the Crypto</div>
+        <div>Make Rewards Better</div>
+        <div>Your Rewards, Your Choice</div>
+      </div>
+    );
+
     const menuextended = (
       <div className="header">
         <div className="left">
           <button onClick={() => this.togglemenu()}>menu</button>
           <button>Who We Are</button>
           <button> What We Do</button>
+          <button> Why We Do</button>
           <button>How We Do It</button>
-          <button>Boards & Committees</button>
+          <button>Our Motto</button>
         </div>
         <div className="right">
           <div>Invest the Cash, Spend the Crypto</div>
